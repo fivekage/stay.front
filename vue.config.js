34c1587ae501ce53eccdb1de5ff0401a6f85c67b
Théo Cramez <theo.cramez@gmail.com>
2022-10-21 15:40:04 +0200

    small addition + ci

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  pwa: {
+    name: 'Stay',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'GenerateSW',
+  }
+}
