b3f80595301f52d959eaefddf3f7ebdeb9903a0e
Théo Cramez <theo.cramez@gmail.com>
2023-01-05 01:37:07 +0100

    add vuetify
    looks good to go to me... i also added material design icons support and automatic component tree-shaking

--- src/main.js
+++ src/main.js
@@ -14,7 +14,26 @@ const firebaseConfig = {
   appId: process.env.VUE_APP_FIREBASE_APP_ID,
 };
 
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(vuetify).mount("#app");
